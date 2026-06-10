cbea8ed4ab3d042e3986818ba888052a7eb84733
binny <binny0x00@gmail.com>
2026-06-11 02:41:50 +0900

    study: 'State와 useState'까지 학습한 내용

--- react-study/my-first-react/src/App.jsx
+++ react-study/my-first-react/src/App.jsx
@@ -9,25 +9,12 @@ import UserCard from './components/UserCard.jsx'
 import Card from './components/Card'
 import Counter from './components/Counter.jsx'
 import User from './components/User.jsx'
-import InputDemo from './components/InputDemo.jsx'
-import SearchBox from './components/SearchBox.jsx'
-import LoginForm from "./components/LoginForm.jsx";
-import Button from "./components/Button.jsx";
-import Toggle from "./components/Toggle.jsx";
-import MessageForm from "./components/MessageForm.jsx";
-import LoginButton from "./components/LoginButton.jsx";
-import UserStatus from "./components/UserStatus.jsx";
-import Notification from "./components/Notification.jsx";
-import Banner from "./components/Banner.jsx";
-import Page from "./components/Page.jsx";
-import FruitList from "./components/FruitList.jsx";
-import TodoList from "./components/TodoList.jsx";
 
 function App() {
-
   return(
     <>
-        <TodoList/>
+      <h1>정보 수정</h1>
+      <User />
     </>
   );
 }
--- react-study/my-first-react/src/components/Greeting.jsx
+++ react-study/my-first-react/src/components/Greeting.jsx
@@ -1,9 +1,5 @@
-function Greeting({user}){
-    if (!user){
-        return <p>로그인이 필요합니다.</p>
-    }
-
-    return <h1>안녕하세요, {user.name}님!</h1>
+function Greeting({name, greeting='안녕하세요'}){
+    return <h1>{greeting}, {name}님!</h1>;
 }
 
 export default Greeting;
