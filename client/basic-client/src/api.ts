2535f9fbd7379612ef51b6a233a924693adf62a3
binny <binny0x00@gmail.com>
2026-06-12 21:14:10 +0900

    feat: client API 헬퍼

---
+++ client/basic-client/src/api.ts
@@ -0,0 +1,34 @@
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export async function login(email: string, password: string) {
+    const response = await fetch(`${API_BASE_URL}/api/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email,
+            password,
+        }),
+    });
+    if (!response.ok) {
+        throw new Error('로그인 실패');
+    }
+}
+
+export async function signup(nickname: string, email: string, password: string) {
+    const response = await fetch(`${API_BASE_URL}/api/signup`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            nickname,
+            email,
+            password,
+        }),
+    });
+    if (!response.ok) {
+        throw new Error('회원가입 실패');
+    }
+}
